049da4fe0ad4eb3447734f24a823aecfb1147e18
Jon Cram <jon@webignition.net>
2019-07-13 12:43:02 +0100

    Create Page

---
+++ src/Page.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace webignition\BasilDataStructure;
+
+class Page extends AbstractDataStructure
+{
+    const KEY_URL = 'url';
+    const KEY_ELEMENTS = 'elements';
+
+    public function getUrl(): string
+    {
+        return $this->getString(self::KEY_URL);
+    }
+
+    public function getElements(): array
+    {
+        return $this->getArray(self::KEY_ELEMENTS);
+    }
+}
