edc420c9583c7796b70050cf0bddcf9680acf1ab
Jon Cram <webignition@gmail.com>
2019-07-13 12:48:10 +0100

    Create Page (#8)

---
+++ tests/Unit/PageTest.php
@@ -0,0 +1,97 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilDataStructure\Tests\Unit;
+
+use webignition\BasilDataStructure\Page;
+
+class PageTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider getUrlDataProvider
+     */
+    public function testGetUrl(Page $pageDataStructure, string $expectedUrlString)
+    {
+        $this->assertSame($expectedUrlString, $pageDataStructure->getUrl());
+    }
+
+    public function getUrlDataProvider(): array
+    {
+        return [
+            'not present' => [
+                'pageDataStructure' => new Page([]),
+                'expectedUrlString' => '',
+            ],
+            'scalar; integer' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_URL => 100,
+                ]),
+                'expectedUrlString' => '100',
+            ],
+            'scalar; float' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_URL => 3.14,
+                ]),
+                'expectedUrlString' => '3.14',
+            ],
+            'scalar; string' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_URL => 'http://example.com/',
+                ]),
+                'expectedUrlString' => 'http://example.com/',
+            ],
+            'scalar; bool, true' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_URL => true,
+                ]),
+                'expectedUrlString' => '1',
+            ],
+            'scalar; bool, false' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_URL => false,
+                ]),
+                'expectedUrlString' => '',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getElementsDataProvider
+     */
+    public function testGetElements(Page $pageDataStructure, array $expectedElementData)
+    {
+        $this->assertSame($expectedElementData, $pageDataStructure->getElements());
+    }
+
+    public function getElementsDataProvider(): array
+    {
+        return [
+            'not present' => [
+                'pageDataStructure' => new Page([]),
+                'expectedElementData' => [],
+            ],
+            'not an array' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_ELEMENTS => 'string',
+                ]),
+                'expectedElementData' => [],
+            ],
+            'empty array' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_ELEMENTS => [],
+                ]),
+                'expectedElementData' => [],
+            ],
+            'non-empty array' => [
+                'pageDataStructure' => new Page([
+                    Page::KEY_ELEMENTS => [
+                        'title' => '.title',
+                    ],
+                ]),
+                'expectedElementData' => [
+                    'title' => '.title',
+                ],
+            ],
+        ];
+    }
+}
