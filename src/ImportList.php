935366bb0bc6181a6c8c1ac1a9178e1254f9aa97
Jon Cram <jon@webignition.net>
2019-07-13 12:47:17 +0100

    Create ImportList

---
+++ src/ImportList.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace webignition\BasilDataStructure;
+
+class ImportList extends AbstractDataStructure
+{
+    private $basePath = '';
+    private $pathResolver;
+
+    public function __construct(PathResolver $pathResolver, string $basePath, array $data)
+    {
+        parent::__construct($this->normalizePaths($data));
+
+        $this->pathResolver = $pathResolver;
+        $this->basePath = $basePath;
+    }
+
+    public function getPaths(): array
+    {
+        return $this->resolvePaths($this->data);
+    }
+
+    private function resolvePaths(array $paths): array
+    {
+        foreach ($paths as $pathIndex => $path) {
+            $paths[$pathIndex] = $this->pathResolver->resolve($this->basePath, $path);
+        }
+
+        return $paths;
+    }
+
+    private function normalizePaths(array $paths): array
+    {
+        foreach ($paths as $pathIndex => $path) {
+            $paths[$pathIndex] = (string) $path;
+        }
+
+        return $paths;
+    }
+}
---
+++ tests/Unit/ImportListTest.php
@@ -0,0 +1,83 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilDataStructure\Tests\Unit;
+
+use webignition\BasilDataStructure\ImportList;
+use webignition\BasilDataStructure\PathResolver;
+
+class ImportListTest extends \PHPUnit\Framework\TestCase
+{
+    public function testEmptyList()
+    {
+        $importList = new ImportList(PathResolver::create(), '', []);
+
+        $this->assertSame([], $importList->getPaths());
+    }
+
+    /**
+     * @dataProvider pathsDataProvider
+     */
+    public function testGetPaths(string $basePath, array $paths, array $expectedPaths)
+    {
+        $importList = new ImportList(PathResolver::create(), $basePath, $paths);
+
+        $this->assertSame($expectedPaths, $importList->getPaths());
+    }
+
+    public function pathsDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'basePath' => '',
+                'paths' => [],
+                'expectedPaths' => [],
+            ],
+            'relative import path, no base path' => [
+                'basePath' => '',
+                'paths' => [
+                    'foo' => '../Relative/foo.yml',
+                ],
+                'expectedPaths' => [
+                    'foo' => '../Relative/foo.yml',
+                ],
+            ],
+            'relative import path, has base path; previous directory' => [
+                'basePath' => '/basil/Test/',
+                'paths' => [
+                    'foo' => '../Relative/foo.yml',
+                ],
+                'expectedPaths' => [
+                    'foo' => '/basil/Relative/foo.yml',
+                ],
+            ],
+            'relative import path, has base path; current directory' => [
+                'basePath' => '/basil/Test/',
+                'paths' => [
+                    'foo' => './Relative/foo.yml',
+                ],
+                'expectedPaths' => [
+                    'foo' => '/basil/Test/Relative/foo.yml',
+                ],
+            ],
+            'absolute import path, no base path' => [
+                'basePath' => '/basil/Test/',
+                'paths' => [
+                    'foo' => '/Absolute/foo.yml',
+                ],
+                'expectedPaths' => [
+                    'foo' => '/Absolute/foo.yml',
+                ],
+            ],
+            'integer' => [
+                'basePath' => '/basil/Test/',
+                'paths' => [
+                    'foo' => 123,
+                ],
+                'expectedPaths' => [
+                    'foo' => '123',
+                ],
+            ],
+        ];
+    }
+}
