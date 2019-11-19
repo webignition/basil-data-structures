7dccf544bd0bce08a1ab1b8b93e8ec2b61a4eca1
Jon Cram <jon@webignition.net>
2019-11-19 17:40:37 +0000

    declare(strict_types=1);

--- src/Test/Imports.php
+++ src/Test/Imports.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace webignition\BasilDataStructure\Test;
 
 use webignition\BasilDataStructure\AbstractDataStructure;
--- tests/Unit/Test/TestTest.php
+++ tests/Unit/Test/TestTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace webignition\BasilDataStructure\Tests\Unit\Test;
 
 use webignition\BasilDataStructure\PathResolver;
