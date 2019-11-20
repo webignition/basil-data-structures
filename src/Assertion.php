2cc720d1a3456abd22373ebd8798d0b49f63c5e2
Jon Cram <webignition@gmail.com>
2019-11-20 13:28:50 +0000

    Add assertion (#22)

---
+++ src/Assertion.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilDataStructure;
+
+class Assertion
+{
+    private $source;
+    private $identifier;
+    private $comparison;
+    private $value;
+
+    public function __construct(string $source, string $identifier, string $comparison, string $value)
+    {
+        $this->source = $source;
+        $this->identifier = $identifier;
+        $this->comparison = $comparison;
+        $this->value = $value;
+    }
+
+    public function getSource(): string
+    {
+        return $this->source;
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getComparison(): string
+    {
+        return $this->comparison;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
