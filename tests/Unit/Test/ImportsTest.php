fc3a138b3464ca31bf044e4b8b3bac1cb5674636
Jon Cram <jon@webignition.net>
2019-11-21 10:13:24 +0000

    Re-implement Test/Imports

--- tests/Unit/Test/ImportsTest.php
+++ tests/Unit/Test/ImportsTest.php
@@ -4,129 +4,64 @@ declare(strict_types=1);
 
 namespace webignition\BasilDataStructure\Tests\Unit\Test;
 
-use webignition\BasilDataStructure\PathResolver;
 use webignition\BasilDataStructure\Test\Imports;
 
 class ImportsTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var PathResolver
-     */
-    private $pathResolver;
-
-    protected function setUp(): void
+    public function testGetWithStepPaths()
     {
-        parent::setUp();
+        $imports = new Imports();
+        $this->assertSame([], $imports->getStepPaths());
 
-        $this->pathResolver = PathResolver::create();
-    }
-
-    public function testEmptyImports()
-    {
-        $importsDataStructure = new Imports($this->pathResolver, '', []);
-
-        $this->assertSame([], $importsDataStructure->getStepPaths());
-        $this->assertSame([], $importsDataStructure->getPagePaths());
-        $this->assertSame([], $importsDataStructure->getDataProviderPaths());
-    }
-
-    /**
-     * @dataProvider pathsDataProvider
-     */
-    public function testGetStepPaths($paths, string $basePath, array $expectedPaths)
-    {
-        $importsDataStructure = new Imports(
-            $this->pathResolver,
-            $basePath,
-            [
-                Imports::KEY_STEPS => $paths,
-            ]
-        );
-
-        $this->assertSame($expectedPaths, $importsDataStructure->getStepPaths());
-    }
-
-    /**
-     * @dataProvider pathsDataProvider
-     */
-    public function testGetPagePaths($paths, string $basePath, array $expectedPaths)
-    {
-        $importsDataStructure = new Imports(
-            $this->pathResolver,
-            $basePath,
-            [
-                Imports::KEY_PAGES => $paths,
-            ]
-        );
-
-        $this->assertSame($expectedPaths, $importsDataStructure->getPagePaths());
-    }
-
-    /**
-     * @dataProvider pathsDataProvider
-     */
-    public function testGetDataProviderPaths($paths, string $basePath, array $expectedPaths)
-    {
-        $importsDataStructure = new Imports(
-            $this->pathResolver,
-            $basePath,
-            [
-                Imports::KEY_DATA_PROVIDERS => $paths,
-            ]
-        );
-
-        $this->assertSame($expectedPaths, $importsDataStructure->getDataProviderPaths());
-    }
-
-    public function pathsDataProvider(): array
-    {
-        return [
-            'not an array' => [
-                'paths' => 'not an array',
-                'basePath' => '',
-                'expectedPaths' => [],
-            ],
-            'empty' => [
-                'paths' => [],
-                'basePath' => '',
-                'expectedPaths' => [],
-            ],
-            'relative import path, no base path' => [
-                'paths' => [
-                    'foo' => '../Relative/foo.yml',
-                ],
-                'basePath' => '',
-                'expectedPaths' => [
-                    'foo' => '../Relative/foo.yml',
-                ],
-            ],
-            'relative import path, has base path; previous directory' => [
-                'paths' => [
-                    'foo' => '../Relative/foo.yml',
-                ],
-                'basePath' => '/basil/Test/',
-                'expectedPaths' => [
-                    'foo' => '/basil/Relative/foo.yml',
-                ],
-            ],
-            'relative import path, has base path; current directory' => [
-                'paths' => [
-                    'foo' => './Relative/foo.yml',
-                ],
-                'basePath' => '/basil/Test/',
-                'expectedPaths' => [
-                    'foo' => '/basil/Test/Relative/foo.yml',
-                ],
-            ],
-            'absolute import path, no base path' => [
-                'paths' => [
-                    'foo' => './Relative/foo.yml',
-                ],
-                'basePath' => '/basil/Test/',
-                'expectedPaths' => [
-                    'foo' => '/basil/Test/Relative/foo.yml',
-                ],
-            ],
+        $paths = [
+            'invalid bool' => true,
+            'invalid int' => 7,
+            'step_name' => '/absolute/path/to/step.yml',
         ];
+
+        $expectedPaths = [
+            'step_name' => '/absolute/path/to/step.yml',
+        ];
+
+        $imports = $imports->withStepPaths($paths);
+        $this->assertSame($expectedPaths, $imports->getStepPaths());
+    }
+
+    public function testGetWithPagePaths()
+    {
+        $imports = new Imports();
+        $this->assertSame([], $imports->getPagePaths());
+
+        $paths = [
+            'invalid bool' => true,
+            'invalid int' => 7,
+            'page_name' => '/absolute/path/to/page.yml',
+        ];
+
+        $expectedPaths = [
+            'page_name' => '/absolute/path/to/page.yml',
+        ];
+
+        $imports = $imports->withPagePaths($paths);
+        $this->assertSame($expectedPaths, $imports->getPagePaths());
+    }
+
+    public function testGetWithDataProviderPaths()
+    {
+        $imports = new Imports();
+        $this->assertSame([], $imports->getDataProviderPaths());
+
+        $paths = [
+            'invalid bool' => true,
+            'invalid int' => 7,
+            'dataProvider_name' => '/absolute/path/to/dataProvider.yml',
+        ];
+
+        $expectedPaths = [
+            'dataProvider_name' => '/absolute/path/to/dataProvider.yml',
+        ];
+
+        $imports = $imports->withDataProviderPaths($paths);
+        $this->assertSame($expectedPaths, $imports->getDataProviderPaths());
     }
 }
