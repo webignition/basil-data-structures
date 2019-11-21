f745631c06de479ce3fccd5348822589d0d69add
Jon Cram <jon@webignition.net>
2019-11-21 10:17:09 +0000

    Re-implement Test/Test

--- src/Test/Test.php
+++ src/Test/Test.php
@@ -4,53 +4,31 @@ declare(strict_types=1);
 
 namespace webignition\BasilDataStructure\Test;
 
-use webignition\BasilDataStructure\AbstractDataStructure;
-use webignition\BasilDataStructure\PathResolver;
-use webignition\BasilDataStructure\Step;
-
-class Test extends AbstractDataStructure
+class Test
 {
-    public const KEY_CONFIGURATION = 'config';
-    public const KEY_IMPORTS = 'imports';
+    private $name = '';
+    private $configuration;
+    private $imports;
 
-    private $path = '';
-    private $pathResolver;
-
-    public function __construct(PathResolver $pathResolver, array $data, string $path = '')
+    public function __construct(string $name, Configuration $configuration, Imports $imports)
     {
-        parent::__construct($data);
+        $this->name = $name;
+        $this->configuration = $configuration;
+        $this->imports = $imports;
+    }
 
-        $this->path = $path;
-        $this->pathResolver = $pathResolver;
+    public function getName(): string
+    {
+        return $this->name;
     }
 
     public function getConfiguration(): Configuration
     {
-        return new Configuration($this->data[self::KEY_CONFIGURATION] ?? []);
+        return $this->configuration;
     }
 
     public function getImports(): Imports
     {
-        $basePath = dirname($this->path) . DIRECTORY_SEPARATOR;
-
-        return new Imports($this->pathResolver, $basePath, $this->data[self::KEY_IMPORTS] ?? []);
-    }
-
-    public function getSteps(): array
-    {
-        $stepNames = array_diff(array_keys($this->data), [self::KEY_CONFIGURATION, self::KEY_IMPORTS]);
-
-        $steps = [];
-
-        foreach ($stepNames as $stepName) {
-            $steps[$stepName] = new Step($this->data[$stepName]);
-        }
-
-        return $steps;
-    }
-
-    public function getPath(): string
-    {
-        return $this->path;
+        return $this->imports;
     }
 }
