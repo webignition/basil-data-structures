2b4f71d6208f6015a677ae380fb07f63847a5d22
Jon Cram <jon@webignition.net>
2019-11-21 15:49:31 +0000

    Add steps to Test

--- tests/Unit/Test/TestTest.php
+++ tests/Unit/Test/TestTest.php
@@ -4,27 +4,70 @@ declare(strict_types=1);
 
 namespace webignition\BasilDataStructure\Tests\Unit\Test;
 
+use webignition\BasilDataStructure\Action\WaitAction;
+use webignition\BasilDataStructure\Assertion;
+use webignition\BasilDataStructure\Step;
 use webignition\BasilDataStructure\Test\Configuration;
 use webignition\BasilDataStructure\Test\Imports;
 use webignition\BasilDataStructure\Test\Test;
 
 class TestTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreate()
+    /**
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(string $path, Configuration $configuration, array $steps, array $expectedSteps)
     {
-        $path = 'test.yml';
-        $configuration = new Configuration('', '');
-
-        $test = new Test($path, $configuration);
+        $test = new Test($path, $configuration, $steps);
 
         $this->assertSame($path, $test->getPath());
         $this->assertSame($configuration, $test->getConfiguration());
+        $this->assertEquals($expectedSteps, $test->getSteps());
         $this->assertEquals(new Imports(), $test->getImports());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'path' => 'test.yml',
+                'configuration' => new Configuration('', ''),
+                'steps' => [],
+                'expectedSteps' => [],
+            ],
+            'valid and invalid steps' => [
+                'path' => 'test.yml',
+                'configuration' => new Configuration('', ''),
+                'steps' => [
+                    1,
+                    'string',
+                    true,
+                    new Step(
+                        [
+                            new WaitAction('wait 1', '1'),
+                        ],
+                        [
+                            new Assertion('".selector" exists', '".selector"', 'exists'),
+                        ]
+                    ),
+                ],
+                'expectedSteps' => [
+                    new Step(
+                        [
+                            new WaitAction('wait 1', '1'),
+                        ],
+                        [
+                            new Assertion('".selector" exists', '".selector"', 'exists'),
+                        ]
+                    )
+                ],
+            ],
+        ];
     }
 
     public function testWithImports()
     {
-        $test = new Test('test.yml', new Configuration('', ''));
+        $test = new Test('test.yml', new Configuration('', ''), []);
         $this->assertEquals(new Imports(), $test->getImports());
 
         $nonEmptyImports = (new Imports())
