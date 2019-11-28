8c4541c10b73cc79826a7b5fed8c8d317436bc88
Jon Cram <jon@webignition.net>
2019-11-28 18:42:00 +0000

    Create StatementInterface

--- src/Action/ActionInterface.php
+++ src/Action/ActionInterface.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace webignition\BasilDataStructure\Action;
 
-interface ActionInterface
+use webignition\BasilDataStructure\StatementInterface;
+
+interface ActionInterface extends StatementInterface
 {
-    public function getSource(): string;
     public function getType(): ?string;
     public function getArguments(): ?string;
 }
--- src/AssertionInterface.php
+++ src/AssertionInterface.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace webignition\BasilDataStructure;
 
-interface AssertionInterface
+interface AssertionInterface extends StatementInterface
 {
-    public function getSource(): string;
     public function getIdentifier(): ?string;
     public function getComparison(): ?string;
     public function getValue(): ?string;
